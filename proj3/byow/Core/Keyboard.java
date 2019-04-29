15aacd4c903887bfc31c496d97e7ee0e78899057
Lauren Hom <laurenhom@berkeley.edu>
2019-04-28 22:29:18 -0700

    updated sollicit

--- proj3/byow/Core/Keyboard.java
+++ proj3/byow/Core/Keyboard.java
@@ -10,7 +10,7 @@ public class Keyboard {
 
     public static void sollicitInput() {
         while (true) {
-            if (StdDraw.hasNextKeyTyped() == true) {
+            if (StdDraw.hasNextKeyTyped()) {
                 char letter = StdDraw.nextKeyTyped();
                 if(letter == 'N' || letter == 'n'){
                     System.out.println("new game ");
@@ -19,17 +19,19 @@ public class Keyboard {
                     System.out.println("load game");
                     //load method
 
-                } else if (letter == ':'){
-//                    char checkQ = StdDraw.nextKeyTyped();
-                    System.out.println("maybe quit?");
-//                    if( checkQ == 'Q' || checkQ == 'q'){
-//                        //quit and save method
-//                        System.out.println("really quit");
-//                    }
-
+                } else if (letter == ':') {
+                    System.out.println("gotta figure out how to quit");
+                    while (true){
+                        if (StdDraw.hasNextKeyTyped()) {
+                            char checkQ = StdDraw.nextKeyTyped();
+                            if (checkQ == 'Q' || checkQ == 'q') {
+                                //quit and save method
+                                System.out.println("really quit now");
+                            }
+                        }
+                    }
 
                 }
-
             }
 
         }
